ef0e50483f3d3136aeca80156695ce4541b1de76
Aldo Sanchez <52171931+aldosanchez2002@users.noreply.github.com>
2026-02-18 15:12:26 -0800

    remove failed v2

--- v2-build/src/components/views/HistoryView.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../../context/AppContext';
-import { CheckCircle, Coffee } from 'lucide-react';
-import { formatCurrency, getLocalDateStr } from '../../utils/helpers';
-
-const HistoryView = () => {
-    // Copy content from original HistoryView
-};
-export default HistoryView;
